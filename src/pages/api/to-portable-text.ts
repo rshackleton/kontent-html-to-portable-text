a3b5d962cc9d83082f40cedba673f7404fe12531
Richard Shackleton <shackleberry112@gmail.com>
2023-02-18 14:51:50 +0000

    feat: add parsing via ondrejs custom parser

--- src/pages/api/to-portable-text.ts
+++ src/pages/api/to-portable-text.ts
@@ -1,24 +1,47 @@
+import { RichTextNodeParser } from '@pokornyd/kontent-ai-rich-text-parser/dist/src/parser/node/index';
 import type { APIRoute } from 'astro';
 
-export const post: APIRoute = async (context) => {
-  const body = await context.request.json();
+type PostBody = {
+  html: string;
+};
 
-  if (!('html' in body)) {
+export const post: APIRoute = async (context) => {
+  const body = (await context.request.json()) as PostBody;
+
+  if (!parseBody(body)) {
     return new Response(null, {
       status: 400,
       statusText: 'Bad Request',
     });
   }
 
-  return {
-    body: JSON.stringify({}),
-  };
+  try {
+    const parser = new RichTextNodeParser();
+    const parsed = parser.parse(body.html);
+
+    return {
+      body: JSON.stringify({ result: parsed }),
+    };
+  } catch (error) {
+    // Rethrow in development
+    if (import.meta.env.DEV) {
+      throw error;
+    }
+
+    return {
+      body: JSON.stringify({ error }),
+    };
+  }
 };
 
-function sleep(duration: number) {
-  return new Promise<void>((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, duration);
-  });
+/**
+ * Parse request body as PostBody
+ */
+function parseBody(input: unknown): input is PostBody {
+  return (
+    typeof input === 'object' &&
+    input !== null &&
+    'html' in input &&
+    typeof input.html === 'string'
+  );
 }
